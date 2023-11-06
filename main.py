bed067121201e746807d9c05c481bceda0015241
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-06 07:08:36 -0300

    Feat: Sunrise and sunset API request

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-from tkinter import *
 import requests
+from datetime import datetime
 
-def get_quote():
-    response = requests.get("https://api.kanye.rest")
-    response.raise_for_status()
-    quote = response.json()["quote"]
-    canvas.itemconfig(quote_text, text=quote)
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()["iss_position"]
 
+parameters = {
+    "lat": -23.962931,
+    "long": -46.392029,
+    "formatted": 0,
+}
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
-
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
-
-
-
-window.mainloop()
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+sunrise = response.json()["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = response.json()["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+time_now = datetime.now()
